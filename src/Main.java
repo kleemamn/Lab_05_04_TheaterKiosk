da9cf5e7747e7e7bbb3deba877abffbbb4d34721
kleemamn <kleemamn@mail.uc.edu>
2022-09-25 20:59:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        final double DRINKING_AGE = 21;
+
+        if(age >= DRINKING_AGE)
+        {
+            System.out.println();
+        }
+    }
+}
